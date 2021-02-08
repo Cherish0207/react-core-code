822dc658050ff17951c9ebb7c35734fa4e705ddb
Cherish0207 <cheshire_1207@foxmail.com>
2021-02-08 20:20:23 +0800

    高阶组件--属性代理,操作props

--- src/index.js
+++ src/index.js
@@ -1,80 +1,37 @@
 import React from "./react/index";
 import ReactDOM from "./react-dom";
-let PersonContext = React.createContext();
-function getStyle(color) {
-  return { border: `5px solid ${color}`, padding: "5px" };
-}
-class Person extends React.Component {
-  state = { color: "red" };
-  changeColor = (color) => this.setState({ color });
-  render() {
-    let contextValue = {
-      color: this.state.color,
-      changeColor: this.changeColor,
+
+let widthLoading = loadingMessage => (OldComponent) => {
+  return class NewComponent extends React.Component {
+    show = () => {
+      let div = document.createElement("div");
+      div.innerHTML = `<p id="loading" style="position: absolute;top:50%;left:50%；z-index:10;background-color:#999;">${loadingMessage}</p>`;
+      document.body.appendChild(div);
     };
-    return (
-      <PersonContext.Provider value={contextValue}>
-        <div className="person" style={getStyle(this.state.color)}>
-          Person
-          <Head></Head>
-          <Body></Body>
-        </div>
-      </PersonContext.Provider>
-    );
-  }
+    hide = () => {
+      document.getElementById("loading").remove();
+    };
+    render() {
+      let extraProps = {
+        show: this.show,
+        hide: this.hide,
+      };
+      return <OldComponent {...this.props} {...extraProps} />;
+    }
+  };
 }
-class Body extends React.Component {
-  static contextType = PersonContext;
+@widthLoading('111加载中...')
+class Hello extends React.Component {
   render() {
     return (
-      <div className="Body" style={getStyle(this.context.color)}>
-        Body
-        <Hands></Hands>
+      <div>
+        <p>hello</p>
+        <button onClick={this.props.show}>show</button>
+        <button onClick={this.props.hide}>hide</button>
       </div>
     );
   }
 }
-function Hands(props) {
-  return (
-    <PersonContext.Consumer>
-      {(contextValue) => (
-        <div className="Hands" style={getStyle(contextValue.color)}>
-          Hands
-          <button onClick={() => contextValue.changeColor("red")}>--red</button>
-          <button onClick={() => contextValue.changeColor("blue")}>
-            --blue
-          </button>
-        </div>
-      )}
-    </PersonContext.Consumer>
-  );
-}
-// class Hands extends React.Component {
-//   static contextType = PersonContext;
-//   render() {
-//     return (
-//       <div className="Hands" style={getStyle(this.context.color)}>
-//         Hands
-//         <button onClick={() => this.context.changeColor("red")}>red</button>
-//         <button onClick={() => this.context.changeColor("blue")}>blue</button>
-//       </div>
-//     );
-//   }
-// }
-class Head extends React.Component {
-  static contextType = PersonContext;
-  render() {
-    return (
-      <div className="Head" style={getStyle(this.context.color)}>
-        Head<Eye></Eye>
-      </div>
-    );
-  }
-}
-class Eye extends React.Component {
-  static contextType = PersonContext;
-  render() {
-    return <div className="eye">Eye</div>;
-  }
-}
-ReactDOM.render(<Person />, document.getElementById("root"));
+// let newLoading = widthLoading('加载中...')
+// let NewHello = newLoading(Hello)
+ReactDOM.render(<Hello />, document.getElementById("root"));
