8be7e7ecbcc844e1f74af343926daa85f46c54f7
Cherish0207 <cheshire_1207@foxmail.com>
2021-02-09 13:47:05 +0800

    hooks--useState用法

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
-import React from "./react/index";
-import ReactDOM from "./react-dom";
-// 默认情况下,只要改了状态,那么所有的组件,不管它的属性变没有,都要更新 
-// 都会dom-diff?
-class Counter extends React.Component {
-  state = { number1: { number: 0 }, number2: { number: 0 } };
-  addNumber1 = () => {
-    let number1 = this.state.number1;
-    number1.number += 1;
-    this.setState({ number1 });
-  };
-  addNumber2 = () => {
-    let number2 = this.state.number2;
-    number2.number += 1; 
-    this.setState({ number2 });
-  };
-  render() {
-    console.log("Counter render");
-    return (
-      <div>
-        <ChildCounter1 number={this.state.number1} />
-        <ChildCounter2 number={this.state.number2} />
-        <button onClick={this.addNumber1}>ChildCounter1 +</button>
-        <button onClick={this.addNumber2}>ChildCounter2 +</button>
-      </div>
-    );
-  }
-}
-class ChildCounter1 extends React.PureComponent {
-  render() {
-    console.log("ChildCounter1 render");
-    return <div>ChildCounter1:{this.props.number.number}</div>;
-  }
-}
-class ChildCounter2 extends React.PureComponent {
-  render() {
-    console.log("ChildCounter2 render");
-    return <div>ChildCounter2:{this.props.number.number}</div>;
-  }
+import React from "react";
+import ReactDOM from "react-dom";
+/**
+ * hooks不能用在if或for循环中
+ */
+function App() {
+  let [number, setNumber] = React.useState(0);
+  return (
+    <div>
+      <p>{number}</p>
+      <button onClick={() => setNumber(number + 1)}>+</button>
+    </div>
+  );
 }
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
