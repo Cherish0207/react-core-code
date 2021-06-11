624afa1599ee87e32d7ded662a7f9fbd44190200
Cherish0207 <cheshire_1207@foxmail.com>
2021-06-11 13:24:04 +0800

    2.利用JavaScript闭包

--- src/ProfilePageClass.js
+++ src/ProfilePageClass.js
@@ -9,17 +9,24 @@ import React from "react";
 
  * 这样的做法破坏了类提供的工程学。同时这也很难让人去记住传递的变量或者强制执行，这也是为什么人们总是在解决bugs。
  */
+// 方法二：利用JavaScript闭包
+/**
+ * 如果你在一次特定的渲染中捕获那一次渲染所用的props或者state，你会发现他们总是会保持一致，就如同你的预期那样：
+ * 你在渲染的时候就已经"捕获"了props：。这样，在它内部的任何代码（包括 showMessage）都保证可以得到这一次特定渲染所使用的props。
+ */
 class ProfilePage extends React.Component {
-  
-  showMessage = (user) => alert("Followed " + user);
-
-  handleClick = () => {
-    const { user } = this.props;
-    setTimeout(() => this.showMessage(user), 3000);
-  };
-
   render() {
-    return <button onClick={this.handleClick}>Follow</button>;
+    const props = this.props;
+
+    const showMessage = () => {
+      alert("Followed " + props.user);
+    };
+
+    const handleClick = () => {
+      setTimeout(showMessage, 3000);
+    };
+
+    return <button onClick={handleClick}>Follow</button>;
   }
 }
 
